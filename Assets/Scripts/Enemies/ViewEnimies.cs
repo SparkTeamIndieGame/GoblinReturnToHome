8863fea7184419ab6876f2ad971dc5dda9f1cb96
SparkTeamIndieGame <spark.teame.indie.games@gmail.com>
2024-05-27 21:08:22 +0300

    G-I-commit#3
    добавил Zjenject

--- Assets/Scripts/Enemies/ViewEnimies.cs
+++ Assets/Scripts/Enemies/ViewEnimies.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class ViewEnimies : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject[] _enemies;
+    [SerializeField] private GameObject _player;
+    [Space (10)]
+    [SerializeField] private float _distancionActivation;
+    [SerializeField] private float _distancionDeactivation;
 
-    // Update is called once per frame
-    void Update()
+    private float currentDistance;
+
+    private void Update()
     {
-        
+        ActiveAndDectivate();
+    }
+    private void ActiveAndDectivate()
+    {
+        for (int i = 0; i < _enemies.Length; i++)
+        {
+            currentDistance = Vector3.Distance(_player.transform.position, _enemies[i].transform.position);
+            if (currentDistance <= _distancionActivation && _enemies[i].activeSelf == false)
+            {
+                _enemies[i].gameObject.SetActive(true);
+            }
+            else if (currentDistance > _distancionDeactivation && _enemies[i].activeSelf == true)
+            {
+                _enemies[i].gameObject.SetActive(false);
+            }
+          
+        }
     }
 }
