64d3698d932238c9185f17114cf0dd41b7a0e2e5
SavDen <zuzinden00@gmail.com>
2024-05-28 18:36:37 +0300

    D-I-commit#2
    Add moved player, follow camera

--- Assets/Scripts/Player/Move.cs
+++ Assets/Scripts/Player/Move.cs
@@ -1,53 +1,62 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Move : MonoBehaviour
 {
-    [SerializeField] private float _speedGround, _jump, _speedAir;
-    private CharacterController characterController;
-    private float move, speed;
-    private int doubleJump = 0;
+    //[SerializeField] private float _speedGround, _jump, _speedAir, _gravity;
+    //private float move, speed;
+    //private int doubleJump = 0;
+    [SerializeField] private float _speed;
+    private CharacterController _characterController;
+    private Vector2 _input;
+    private Vector3 _direction;
 
     private void Awake()
     {
-        characterController = GetComponent<CharacterController>();
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
+        _characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        move = Input.GetAxis("Horizontal");
+        _characterController.Move(_direction * _speed * Time.deltaTime);
+        //move = Input.GetAxis("Horizontal");
 
-        if(characterController.isGrounded)
-        {
-            doubleJump = 0;
-            speed = _speedGround;
-        }
+        //if (characterController.isGrounded)
+        //{
+        //    doubleJump = 0;
+        //    speed = _speedGround;
+        //}
+        //else if (!characterController.isGrounded)
+        //    characterController.Move(new Vector3(0, _gravity, 0) * Time.fixedDeltaTime);
 
-        Jump(Input.GetKeyDown(KeyCode.Space));
-            
+        //Jump(Input.GetKeyDown(KeyCode.Space));
+
 
     }
 
-    private void FixedUpdate()
+    //private void FixedUpdate()
+    //{
+    //    characterController.Move(new Vector3(move, 0, 0) * _speedGround * Time.deltaTime);
+
+    //}
+
+    //private void Jump(bool space)
+    //{
+    //    if (space && doubleJump < 2)
+    //    {
+    //        characterController.Move(Vector3.up * _jump * Time.fixedDeltaTime);
+    //        speed = _speedAir;
+    //        doubleJump++;
+    //    }
+    //    else return;
+
+
+    //}
+
+    public void Moved(InputAction.CallbackContext context)
     {
-        characterController.SimpleMove(new Vector3(move * speed, 0, 0));
-        
-    }
-
-    private void Jump(bool space)
-    {
-        if (space && doubleJump < 2)
-        {
-            characterController.Move(Vector3.up * _jump);
-            speed = _speedAir;
-            doubleJump++;
-        }
-        else return;
-
-
+        _input = context.ReadValue<Vector2>();
+        _direction = new Vector3(_input.x, 0, 0);
     }
 }
