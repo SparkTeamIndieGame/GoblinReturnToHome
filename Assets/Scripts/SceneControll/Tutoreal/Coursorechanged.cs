bf9df57f48edd5380dd58e455a1266e27489b5a8
SparkTeamIndieGame <spark.teame.indie.games@gmail.com>
2024-06-11 00:05:42 +0300

    G-III-commit#24
    Создание туториала

---
+++ Assets/Scripts/SceneControll/Tutoreal/Coursorechanged.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Coursorechanged : MonoBehaviour
+{
+    [SerializeField] private Texture2D _cursoreWeapon;
+    [SerializeField] private Texture2D _cursoreMain;
+
+    public void UseWeaponCursore()
+    {
+        Cursor.SetCursor(_cursoreWeapon, new Vector2(31.0f, 32.0f), CursorMode.Auto);
+    }
+    public void UseMainCursore()
+    {
+        Cursor.SetCursor(_cursoreMain, Vector2.zero, CursorMode.Auto);
+    }
+    public void LoseCursore(bool value)
+    {
+        Cursor.visible = value;
+    }
+}
