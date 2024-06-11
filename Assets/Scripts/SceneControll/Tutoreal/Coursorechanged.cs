14af75fccf484c9028b988196ed2e93cbed2228e
SparkTeamIndieGame <spark.teame.indie.games@gmail.com>
2024-06-11 20:15:25 +0300

    G-III-commit#28
    настройка курсоров

--- Assets/Scripts/SceneControll/Tutoreal/Coursorechanged.cs
+++ Assets/Scripts/SceneControll/Tutoreal/Coursorechanged.cs
@@ -4,7 +4,10 @@ public class Coursorechanged : MonoBehaviour
 {
     [SerializeField] private Texture2D _cursoreWeapon;
     [SerializeField] private Texture2D _cursoreMain;
-
+    private void Start()
+    {
+        UseMainCursore();
+    }
     public void UseWeaponCursore()
     {
         Cursor.SetCursor(_cursoreWeapon, new Vector2(31.0f, 32.0f), CursorMode.Auto);
