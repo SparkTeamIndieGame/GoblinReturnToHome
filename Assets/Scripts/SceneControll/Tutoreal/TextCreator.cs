41c5a015f2f583bf637fbb7c1197a5bddc084ee3
SparkTeamIndieGame <spark.teame.indie.games@gmail.com>
2024-06-21 01:05:13 +0300

    G-V-commit#47
    Создание отдельного тутор уровня

--- Assets/Scripts/SceneControll/Tutoreal/TextCreator.cs
+++ Assets/Scripts/SceneControll/Tutoreal/TextCreator.cs
@@ -7,18 +7,24 @@ public class TextCreator : MonoBehaviour
     [SerializeField] private Text _text;
     [SerializeField] private float _timeBetween;
     [SerializeField] private GameObject _button;
-    private string titulText = "Ура! Вы нашли первый бонус! \r\nСоберайте бонусы, расположеные на уровне." +
+    [TextArea]
+    [SerializeField] private string _userText;
+    private string defaultText = "Ура! Вы нашли первый бонус! \r\nСоберайте бонусы, расположеные на уровне." +
                                 "\r\nСреди них Вы найдете Пополнение здоровья, новое оружие, боеприпасы!\t";
 
     private void Start()
     {
+        if (_userText != string.Empty)
+        {
+            defaultText = _userText;
+        }
         StartCoroutine(TextAnim());
     }
     private IEnumerator TextAnim()
     {
-        for (int i = 0; i < titulText.Length; i++)
+        for (int i = 0; i < defaultText.Length; i++)
         {
-            _text.text += titulText[i].ToString();
+            _text.text += defaultText[i].ToString();
             yield return new WaitForSeconds(_timeBetween);
         }
         _button.SetActive(true);
