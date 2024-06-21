bf1d3dbbc852f72f8be4b20471a7d1af7bd26e18
SavDen <zuzinden00@gmail.com>
2024-06-21 23:18:29 +0300

    D-I-commit#30
    add finish panel

--- Assets/Scripts/SceneControll/FinishScore.cs
+++ Assets/Scripts/SceneControll/FinishScore.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class FinishScore : MonoBehaviour
 {
@@ -9,6 +10,13 @@ public class FinishScore : MonoBehaviour
 
     [SerializeField] private Text _damageText;
     [SerializeField] private Text _killText;
+    [SerializeField] private Text _rank;
+    [SerializeField] private List<string> _listRank;
+
+    /// <summary>
+    /// Интервалы выставляются попарно. Пример убито от 0 до 3, то есть _listRangeEnemy[0] = 0, _listRangeEnemy[1] = 3;
+    /// </summary>
+    [SerializeField] private List<int> _listRangeEnemy;
     
     public void UpdateText()
     {
@@ -24,5 +32,18 @@ public class FinishScore : MonoBehaviour
             _killText.text = _needCountEnemy.GetKillScore().ToString();
 
         }
+
+        if (_scoreControl.GetKillScore() <= _listRangeEnemy[1])
+            _rank.text = _listRank[0];
+
+        else if (_scoreControl.GetKillScore() > _listRangeEnemy[1] && _scoreControl.GetKillScore() <= _listRangeEnemy[2])
+            _rank.text = _listRank[1];
+
+        else if (_scoreControl.GetKillScore() > _listRangeEnemy[2] && _scoreControl.GetKillScore() <= _listRangeEnemy[3])
+            _rank.text = _listRank[2];
+
+        else if (_scoreControl.GetKillScore() > _listRangeEnemy[3])
+            _rank.text = _listRank[3];
+
     }
 }
