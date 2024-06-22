5c60f1d5f31a5122aab0d71b3d92eac102889c08
SparkTeamIndieGame <spark.teame.indie.games@gmail.com>
2024-06-22 23:29:49 +0300

    G-V-commit#49
    TutorealLvl

--- Assets/Scripts/Obstacle/Disk.cs
+++ Assets/Scripts/Obstacle/Disk.cs
@@ -8,47 +8,52 @@ public class Disk : MonoBehaviour
     [SerializeField] private float _speed, _speedRortation;
     [SerializeField] private List<Transform> _point;
     private Vector3 _leftPoint, _rightPoint;
-    private bool _left;
+    private bool left;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         _leftPoint = _point[0].position;
         _rightPoint = _point[1].position;
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         if (transform.position == _leftPoint)
         {
-            _left = false;
+            left = false;
             transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y * -1, transform.localScale.z);
-            _speedRortation *= -1;
 
+            _speedRortation *= -1;
         }
 
         else if (transform.position == _rightPoint)
         {
-            _left = true;
+            left = true;
             if (transform.localScale.y < 0)
+            {
                 transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y * -1, transform.localScale.z);
+            }
             if (_speedRortation < 0)
+            {
                 _speedRortation *= -1;
+            }
         }
-
 
         TransformPosition();
         Rotation();
-
     }
 
     private void TransformPosition()
     {
-        if (_left)
+        if (left)
+        {
             transform.position = Vector3.MoveTowards(transform.position, _leftPoint, _speed * Time.deltaTime);
-        else if (!_left)
+        }
+        else if (!left)
+        {
             transform.position = Vector3.MoveTowards(transform.position, _rightPoint, _speed * Time.deltaTime);
-
+        }
     }
 
     private void Rotation()
