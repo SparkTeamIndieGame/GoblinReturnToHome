13fb65255bfe6f5a5f2df11135eb5003a113dd9d
SavDen <zuzinden00@gmail.com>
2024-07-15 13:08:43 +0300

    Final Change

--- Assets/Scripts/Audio/AudioPause.cs
+++ Assets/Scripts/Audio/AudioPause.cs
@@ -11,38 +11,44 @@ public class AudioPause : MonoBehaviour
     [SerializeField] private AudioMixerGroup _mixer;
     [SerializeField] private Color32[] _color; //0 green, 1 red
 
-    private bool _musOn;
 
-    
+    private static bool MusOn = true;
 
 
     void Start()
     {
-        if(YG.YandexGame.savesData.isFirstSession)
+        
+        if(MusOn)
         {
             _image.sprite = _sprite[0];
             _image.color = _color[0];
-            _musOn = true;
         }
+
+        else if(!MusOn)
+        {
+            _image.sprite = _sprite[1];
+            _image.color = _color[1];
+        }
+
 
 
     }
 
     public void MusButton ()
     {
-        if(_musOn)
+        if(MusOn)
         {
             _image.sprite = _sprite[1];
             _image.color = _color[1];
             _mixer.audioMixer.SetFloat("volume", -80);
-            _musOn = false;
+            MusOn = false;
         }
         else
         {
             _image.sprite = _sprite[0];
             _image.color = _color[0];
             _mixer.audioMixer.SetFloat("volume", 0);
-            _musOn = true;
+            MusOn = true;
         }
     }
 }
